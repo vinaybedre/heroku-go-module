36bf765463ecdc6dec6430303ed303a665890c07
Vinay Bedre <vinaybedre@gmail.com>
2020-03-15 19:15:38 +0530

    add websocket

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/vinaybedre/heroku-go-module
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/websocket v1.4.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,22 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-func indexHandler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(rw, "Hello, Heroku!")
+func main() {
+	portEnv := os.Getenv("PORT")
+	// if len(portEnv) != 0 {
+	// 	portEnv = ":8081"
+	// }
+	r := GetHandler("/ws")
+	r.Run(portEnv)
 }
 
-func main() {
-	// Heroku supplies your port via environment variable
-	port := os.Getenv("PORT")
+func GetHandler(url string) *gin.Engine {
+	r := gin.Default()
+	r.GET(url, func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, "{msg:'its running'}")
+	})
+	return r
+}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	wsupgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := wsupgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	for {
+		// Read message from browser
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		// Print the message to the console
+		fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+		// Write message back to browser
+		if err = conn.WriteMessage(msgType, msg); err != nil {
+			return
+		}
+	}
 }
